6346928ca54a10a8961c7b7853410cb7ba74a41d
harshi3010 <harshadajain3010@gmail.com>
2021-07-20 16:32:35 +0530

    first commit

---
+++ src/expense__tracker/index.js
@@ -0,0 +1,10 @@
+export { default as Splash }
+from "./Svgs/Splash";
+export { default as Chart }
+from "./Svgs/Chart";
+export { default as AddIcon }
+from "./Svgs/AddIcon";
+export { default as BackArrow }
+from "./Svgs/BackArrow";
+export { default as Delete }
+from "./Svgs/Delete";
